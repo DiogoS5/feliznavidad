16b1c5ea96b097bbe915410eff0fcba69477ce10
DiogoS5 <diogomppsousa@gmail.com>
2023-12-03 23:23:58 +0000

    day 3 part 1 working

--- tres/tres.py
+++ tres/tres.py
@@ -1,47 +1,67 @@
-engine = open("tres\\engine.txt")
 
+engine = open("tres\\input_sosa.txt", "r")
 
-def has_symbol(iline, numindex, numlen):
-    symbols = ["*", "=", "/", "@", "$", "%", "&", "#"]
-    symbol = False
-    if iline == 0:
-        cline = engine.readline(iline)
-        above = engine.readline(iline)
-        pline = above[numindex - 1, numlen + 1]
-        if numindex == 0:
+matrix = []
+symbols = ["*", "=", "/", "@", "$", "%", "&", "#", "+", "-"]
 
-            symbol = (cline[numindex + numlen] != ".") or any(substring in pline for substring in symbols)
-        elif numindex == len(line):
-            symbol = (cline[numindex - 1] != ".")
-        else:
-            symbol = (cline[numindex + numlen] != ".") or (cline[numindex - 1] != ".")
+#any(substring in linha for substring in symbols) -> bool
 
-    return symbol
-
-
-partsSum = 0
-iline = 0
 for line in engine:
-    i = 0
+    matrix.append(list(line.strip()))
+
+engine.close()
+
+def hasAdjacent(numIndex, iline, numlen):
+    if numIndex > 0:
+        range_start = numIndex - 1
+        #check adjacent to the left
+        if matrix[iline][numIndex - 1] in symbols:
+            return True
+    else:
+        range_start = numIndex
+    
+    if numIndex + numlen < (len(line)):    
+        range_end = numIndex + numlen + 1
+        #check adjacent to the right
+        if matrix[iline][numIndex + numlen] in symbols:
+            return True
+    else:
+        range_end = numIndex + numlen
+    
+    if iline > 0: 
+        for i in range(range_start, range_end):
+            if matrix[iline-1][i] in symbols:
+                return True
+    if iline < len(matrix)-1:
+        for i in range(range_start, range_end):
+            if matrix[iline+1][i] in symbols:
+                return True
+            
+    return False
+
+
+result = 0
+iline = 0
+for line in matrix:
     consecutive = False
-    numberIndex = 0
+    number = ""
+    i = 0
     for x in line:
-        i += 1
-        number = ""
-        # consecutive numbers
         if x.isnumeric() and consecutive:
             number += x
-        # new number
         elif x.isnumeric():
             number = x
-            numberIndex = i
             consecutive = True
-        # number ended / no number found yet
-        if not x.isnumeric:
+            numIndex = i
+        if not x.isnumeric() or i == len(line) - 1:
             consecutive = False
-            number = ""
-            if len(number) != 0:
-                if has_symbol(iline, numberIndex, len(number)):
-                    partsSum += int(number)
+            if len(number) > 0:
+                if hasAdjacent(numIndex, iline, len(number)):
+                    result += int(number)
+            number = ""   
+        i += 1
     iline += 1
-print(partsSum)
+
+print(result)
+
+#488618 low
