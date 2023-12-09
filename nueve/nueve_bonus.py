00d79ba093cbe637a5149862b3d116bf123f8d1c
DiogoS5 <diogomppsousa@gmail.com>
2023-12-09 16:27:05 +0000

    nueve done

---
+++ nueve/nueve_bonus.py
@@ -0,0 +1,27 @@
+import re  
+
+input = open(r"input.txt", "r")
+
+og_lines = []
+for line in input:
+    og_lines.append(line.strip().split(" "))
+
+result = 0
+for line in og_lines:
+    line = [int(i)  for i in line] #convert to ints
+    pyramid = []
+    pyramid.append(line)
+    for p in range(len(line)):
+        deltas = []
+        for i in range(1, len(pyramid[p])):
+            deltas.append(pyramid[p][i]-pyramid[p][i-1])
+        if all([i == 0 for i in deltas]):
+            break
+        pyramid.append(deltas)
+
+    for p in range(len(pyramid)-1, 0, -1):
+        pyramid[p-1].insert(0, pyramid[p-1][0]-pyramid[p][0])
+
+    result += pyramid[0][0]
+
+print(result)
